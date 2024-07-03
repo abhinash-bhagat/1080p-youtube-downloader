b3ad882c3539c81dbff485d815c7336424dc5ad0
Abhinash Bhagat <bhagatabhinash143@gmail.com>
2024-07-03 22:45:21 +0545

    added port number

--- app.py
+++ app.py
@@ -42,4 +42,5 @@ def download_video():
         return jsonify({'error': str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
